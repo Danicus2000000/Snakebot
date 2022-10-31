deed17212fbcbdf909697ded5a5d5c6ef9e8854f
Danicus2000000 <66948931+Danicus2000000@users.noreply.github.com>
2022-10-31 00:56:11 +0000

    Fixed admin commands

--- Snakebot/Commands/slashcommands.cs
+++ Snakebot/Commands/slashcommands.cs
@@ -4,8 +4,8 @@ namespace Snakebot.Commands
 {
     public class slashcommands : ApplicationCommandModule
     {
-        [SlashCommand("logout","Logs out the bot")]
-        [SlashRequirePermissions(DSharpPlus.Permissions.Administrator)]
+        [SlashCommand("logout","Logs out the bot (Requires Admin)")]
+        [SlashRequireUserPermissions(DSharpPlus.Permissions.Administrator)]
         public async Task logout(InteractionContext ctx) 
         {
             await ctx.CreateResponseAsync("Snakebot has logged out!");//goodbye message
@@ -13,8 +13,8 @@ namespace Snakebot.Commands
             Environment.Exit(0);//kill program
         }
 
-        [SlashCommand("Help", "Explains what the bot can do!")]
-        [SlashRequirePermissions(DSharpPlus.Permissions.Administrator)]
+        [SlashCommand("Help", "Explains what the bot can do (Requires Admin)!")]
+        [SlashRequireUserPermissions(DSharpPlus.Permissions.Administrator)]
         public async Task help(InteractionContext ctx)
         {
             await ctx.CreateResponseAsync("Snakebot has one command:\n/logout - (Requires Server Administrator) Logs out the bot and terminates the local client");//goodbye message
